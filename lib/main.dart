bc657c2e373645a8254485da179151e429ea8c3a
SIKV <sv.kiyakh@gmail.com>
2018-12-13 00:08:38 +0200

    Secret, SecretLoader classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:news/home.dart';
 
-void main() => runApp(NewsApp());
+Secret secret;
+
+void main() {
+  SecretLoader(secretPath: "secrets.json").load().then((result) {
+    secret = result;
+  });
+  
+  runApp(NewsApp());
+}
 
 class NewsApp extends StatelessWidget {
   @override
@@ -10,5 +20,28 @@ class NewsApp extends StatelessWidget {
       title: "News",
       home: Home(),
     );
+  }
+}
+
+class Secret {
+  final String apiKey;
+
+  Secret({this.apiKey = ""});
+
+  factory Secret.fromJson(Map<String, dynamic> jsonMap) {
+    return new Secret(apiKey: jsonMap["api_key"]);
+  }
+}
+
+class SecretLoader {
+  final String secretPath;
+
+  SecretLoader({this.secretPath});
+
+  Future<Secret> load() {
+    return rootBundle.loadStructuredData<Secret>(this.secretPath, (jsonStr) async {
+      final secret = Secret.fromJson(json.decode(jsonStr));
+      return secret;
+    });
   }
 }
