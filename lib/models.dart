2adbea69e63cc91633268d33c84c80a93307eb7d
SIKV <sv.kiyakh@gmail.com>
2018-12-16 01:44:12 +0200

    Article card, title without source and formatted date

--- lib/models.dart
+++ lib/models.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class NewsResponse {
   final String status;
   final int totalResults;
@@ -33,12 +35,36 @@ class Article {
   Article({this.source, this.author, this.title, this.description,
     this.url, this.urlToImage, this.publishedAt, this.content});
 
+  String getTitle() {
+    int indexOf = title.lastIndexOf('-');
+
+    if (indexOf != -1) {
+      return title.substring(0, indexOf);
+    } else {
+      return title;
+    }
+  }
+
+  String getPublishedAt() {
+    DateTime dateTime = DateTime.parse(publishedAt);
+    DateTime now = DateTime.now();
+
+    String pattern = '';
+
+    if (dateTime.day == now.day && dateTime.month == now.month && dateTime.year == now.year) {
+      pattern = 'hh:mm a';
+    } else {
+      pattern = 'MM/dd/yy hh:mm a';
+    }
+    return DateFormat(pattern).format(dateTime);
+  }
+
   factory Article.fromJson(Map<String, dynamic> json) {
     return Article(
       source: Source.fromJson(json['source']),
       author: json['author'],
       title: json['title'],
-      description: json['description'],
+      description: json['description'] == null ? "" : json['description'],
       url: json['url'],
       urlToImage: json['urlToImage'] == null ? "" : json['urlToImage'],
       publishedAt: json['publishedAt'],
@@ -56,7 +82,7 @@ class Source {
   factory Source.fromJson(Map<String, dynamic> json) {
     return Source(
       id: json['id'],
-      name: json['name']
+      name: json['name'] == null ? "" : json['name']
     );
   }
 }
--- lib/news_tab.dart
+++ lib/news_tab.dart
@@ -37,8 +37,11 @@ class _NewsTabState extends State<NewsTab> {
       child: ListView.builder(
           itemCount: articles.length,
           itemBuilder: (context, i) => Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0)
+              ),
               margin: EdgeInsets.all(8.0),
-              elevation: 2.0,
+              elevation: 0.5,
               child: InkWell(
                 onTap: () {
                   Navigator.push(
@@ -48,34 +51,58 @@ class _NewsTabState extends State<NewsTab> {
                       )
                   );
                 },
-                child: Stack(
+                child: Container(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Image(
-                        image: NetworkImage(articles[i].urlToImage),
-                        fit: BoxFit.cover,
-                        height: 250.0,
-                      ),
-                      Positioned(
-                        child: Container(
-                          child: Text(
-                              articles[i].title,
-                              style: TextStyle(
-                                  fontSize: 16.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w900
-                              )
-                          ),
-                          decoration: BoxDecoration(
-                              color: Colors.black.withAlpha(150)
-                          ),
-                          padding: EdgeInsets.all(8.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16.0),
+                            topRight: Radius.circular(16.0)
                         ),
-                        left: 0.0,
-                        right: 0.0,
-                        bottom: 0.0,
+                        child: Image(
+                          image: NetworkImage(articles[i].urlToImage),
+                          fit: BoxFit.cover,
+                          height: 175.0,
+                        ),
                       ),
-                    ]
-                ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
+                        child: Text(
+                          articles[i].getTitle(),
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 16.0, top: 8.0, right: 16.0),
+                        child: Text(
+                          articles[i].description,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0, bottom: 16.0),
+                        child: Text(
+                          '${articles[i].source.name} • ${articles[i].getPublishedAt()}',
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                )
               )
           )
       ),
