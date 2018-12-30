9dcb117927f660bd3384cd802cb128ab881c6b12
SIKV <sv.kiyakh@gmail.com>
2018-12-31 01:00:16 +0200

    NewsTab refactor

--- lib/presentation/news_tab.dart
+++ lib/presentation/news_tab.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flux/flutter_flux.dart';
 import 'package:news/actions/actions.dart';
 import 'package:news/models/models.dart';
+import 'package:news/presentation/article_card.dart';
 import 'package:news/stores/news_store.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -50,81 +51,17 @@ class _NewsTabState extends State<NewsTab> with StoreWatcherMixin<NewsTab> {
           if (newsStore.hasMore && i == newsStore.articles.length - 1) {
             loadMoreNewsAction.call();
           }
-          return _articleCard(newsStore.articles[i]);
-        }
-      )
-    );
-  }
 
-  Widget _articleCard(Article article) {
-    return Card(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16)
-        ),
-        margin: EdgeInsets.all(8),
-        elevation: 0.5,
-        child: InkWell(
-            borderRadius: BorderRadius.circular(16),
-            onTap: () {
+          Article article = newsStore.articles[i];
+
+          return ArticleCard(
+            article: article,
+            onPressed: () {
               _openArticle(article);
             },
-            child: Container(
-                child: _articleWidget(article)
-            )
-        )
-    );
-  }
-
-  Widget _articleWidget(Article article) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16)
-          ),
-          child: Image(
-            image: NetworkImage(article.urlToImage),
-            fit: BoxFit.cover,
-            height: 175,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 16, top: 16, right: 16),
-          child: Text(
-            article.getTitle(),
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 16, top: 8, right: 16),
-          child: Text(
-            article.description,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 14,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 16),
-          child: Text(
-            '${article.source.name} • ${article.getPublishedAt()}',
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-      ],
+          );
+        }
+      )
     );
   }
 }
