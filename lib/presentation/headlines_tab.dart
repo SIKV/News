f628029cd7df8d4171cf24d07ce313eea97f5cf3
SIKV <sv.kiyakh@gmail.com>
2019-01-06 21:53:14 +0200

    HeadlinesTab SliverAppBar

--- lib/presentation/headlines_tab.dart
+++ lib/presentation/headlines_tab.dart
@@ -9,42 +9,54 @@ class HeadlinesTab extends StatefulWidget {
 }
 
 class _HeadlinesTabState extends State<HeadlinesTab> with StoreWatcherMixin<HeadlinesTab> {
+  final ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 8,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Headlines'),
-          centerTitle: true,
-          bottom: TabBar(
-            isScrollable: true,
-            tabs: [
-              Tab(text: 'All'),
-              Tab(text: 'Business'),
-              Tab(text: 'Entertainment'),
-              Tab(text: 'General'),
-              Tab(text: 'Health'),
-              Tab(text: 'Science'),
-              Tab(text: 'Sports'),
-              Tab(text: 'Technology'),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.grey.shade100,
-        body: TabBarView(
-          children: <Widget>[
-            NewsTab(category: 'all'),
-            NewsTab(category: 'business'),
-            NewsTab(category: 'entertainment'),
-            NewsTab(category: 'general'),
-            NewsTab(category: 'health'),
-            NewsTab(category: 'science'),
-            NewsTab(category: 'sports'),
-            NewsTab(category: 'technology'),
-          ],
+        length: 8,
+        child: Scaffold(
+            body: NestedScrollView(
+                controller: scrollController,
+                headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    SliverAppBar(
+                        title: Text('Headlines'),
+                        pinned: true,
+                        floating: true,
+                        forceElevated: true,
+                        elevation: 1.0,
+                        centerTitle: true,
+                        bottom: TabBar(
+                            isScrollable: true,
+                            tabs: [
+                              Tab(text: 'All'),
+                              Tab(text: 'Business'),
+                              Tab(text: 'Entertainment'),
+                              Tab(text: 'General'),
+                              Tab(text: 'Health'),
+                              Tab(text: 'Science'),
+                              Tab(text: 'Sports'),
+                              Tab(text: 'Technology'),
+                            ]
+                        )
+                    )
+                  ];
+                },
+                body: TabBarView(
+                  children: <Widget>[
+                    NewsTab(category: 'all'),
+                    NewsTab(category: 'business'),
+                    NewsTab(category: 'entertainment'),
+                    NewsTab(category: 'general'),
+                    NewsTab(category: 'health'),
+                    NewsTab(category: 'science'),
+                    NewsTab(category: 'sports'),
+                    NewsTab(category: 'technology'),
+                  ],
+                )
+            )
         )
-      ),
     );
   }
 }
